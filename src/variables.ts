b4bf2762867f042a4e5a5ca6eb3043fbe550340f
Will Smith <willsmithte@gmail.com>
2022-01-05 09:59:50 +0100

    use default channel with prod env vars

--- src/variables.ts
+++ src/variables.ts
@@ -13,10 +13,12 @@ const ENV = {
 };
 
 function getEnvVars(env = "") {
+  console.debug(`env=${env}`)
   if (env === null || env === undefined || env === "") return ENV.dev;
   if (env.indexOf("dev") !== -1) return ENV.dev;
   if (env.indexOf("staging") !== -1) return ENV.staging;
   if (env.indexOf("prod") !== -1) return ENV.prod;
+  if (env.indexOf("default") !== -1) return ENV.prod;
 }
 
 export const ENV_VARS = getEnvVars(Constants.manifest!!.releaseChannel)!!;
